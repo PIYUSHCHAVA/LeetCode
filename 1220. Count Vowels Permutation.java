25ec25bce7c773f0a6ab8e3b21314844c04080ff
Piyush Chavan <72214414+PIYUSHCHAVA@users.noreply.github.com>
2023-10-28 23:03:59 +0530

    Add files via upload

---
+++ 1220. Count Vowels Permutation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countVowelPermutation(int n) 
+    {
+        long prevA=1, prevE=1, prevI=1, prevO=1, prevU=1;
+        int mod=1000000007;
+        for(int length=2; length<=n; length++)
+        {
+            long nextA=(prevE + prevU + prevI) % mod;
+            long nextE=(prevA + prevI) % mod;
+            long nextI=(prevE + prevO) % mod;
+            long nextO=prevI;
+            long nextU=(prevO + prevI) % mod;
+
+            //update to previous values for the next iterations
+            prevA=nextA;
+            prevE=nextE;
+            prevI=nextI;
+            prevO=nextO;
+            prevU=nextU;
+            
+        }
+        return (int) ((prevA + prevE + prevI + prevO + prevU)% mod);
+    }
+}
