222643e39b191216e0aec3fc31b7441efa51095a
Piyush Chavan <72214414+PIYUSHCHAVA@users.noreply.github.com>
2024-02-02 12:26:01 +0530

    Add files via upload

---
+++ 1291. Sequential Digits.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high)
+    {
+     
+     List<Integer> res=new ArrayList<>();
+
+     for(int i=1; i<=9; i++)
+     {
+         int nums=i;
+         for(int j=i+1; j<=9; j++)
+         {
+             nums=nums * 10 + j;
+             if(nums >= low && nums <= high)
+             {
+                 res.add(nums);
+             }
+         }
+     }
+        Collections.sort(res);
+        return res;
+    }
+}
