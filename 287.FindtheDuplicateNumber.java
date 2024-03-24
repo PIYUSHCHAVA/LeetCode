b7d05c3ea343f2f799c656f6b0bbec0ad2b30167
Piyush Chavan <72214414+PIYUSHCHAVA@users.noreply.github.com>
2024-03-24 10:46:51 +0530

    Add files via upload

---
+++ 287.FindtheDuplicateNumber.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class FindtheDuplicateNumber 
+{
+  public static int duplicateNumber(int [] nums)
+  {
+    Arrays.sort(nums);
+    for(int i=0; i<nums.length-1; i++)
+    {
+        if(nums[i] ==  nums[i+1])
+        {
+            return nums[i];
+        }
+    }
+    return -1;
+  } 
+  public static void main(String[] args)
+  {
+     int nums[]={1,3,4,2,2};
+     System.out.println(duplicateNumber(nums));
+  }  
+}
